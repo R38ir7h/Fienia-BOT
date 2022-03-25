f732d1065c4cf550c1a3e9b1919d55e671a35e84
edc2k19 <forsaken.world.rebirth@gmail.com>
2022-03-25 11:07:39 +0300

    25.03.2022
    25.03.2022

---
+++ config.py
@@ -0,0 +1,46 @@
+# Токены ВК и Discord
+
+vk_access_token = 'TOKEN1'
+discord_token = 'TOKEN2'
+
+game_name = 'Rebirth'
+
+# Настройки VK -> Discord
+
+vk_to_discord_timeout = 90  # Новый пост из ВК каждые 10 минут (600 секунд)
+
+vk_group_ids = [
+    139358441,  # Forsaken World | Rebirth
+
+]
+
+vk_bad_words = {
+    -31480508: ['Длиннопост от ', 'Комментарии: ', 'Длиннопост: '], 
+}
+
+discord_channel_id = 953584737178505226
+
+vk_embed_footer = 'Copyright © 2017-2022 Сообщество fw-rebirth.com'
+
+# Настройки Discord -> Discord
+
+discord_channel_ids = [
+    500000000000000000,
+]
+
+discord_bad_words = [
+    '@here',
+    '@everyone'
+]
+
+discord_urls = {
+    944005578349490216: 'https://discord.gg/python',
+}
+
+add_thumbs = True
+
+discord_to_discord_channel_id = 600000000000000001
+
+# Настройки Embed
+
+embed_color = 0xFF9900
---
+++ gil.py
@@ -0,0 +1,87 @@
+import discord
+import config
+import datetime
+from vk_client import VKClient
+from embed_preparer import prepare_embed_from_vk, prepare_embed_from_discord
+from storage import Storage
+import asyncio
+from tprint import log
+from webserver import keep_alive
+class GillyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.discord_channel = True
+        self.discord_channel = True
+        self.discord_to_discord_channel = True
+        self.vk = self.loop.create_task(self.vk_task())
+
+    @staticmethod
+    async def set_thumbs(message):
+        for reaction in ['👍']:
+            await asyncio.sleep(0.1)
+            await message.add_reaction(reaction)
+
+    async def send_embed_from_vk(self, post, group_profile):
+        embed = prepare_embed_from_vk(post, group_profile)
+        sent = await self.discord_channel.send(embed=embed)
+        if config.add_thumbs:
+            await self.set_thumbs(sent)
+          
+
+    @staticmethod
+    async def get_vk_data(send_method):
+        with Storage(config.vk_group_ids) as s:
+            for _ in config.vk_group_ids:
+                oldest_group, after = s.get_next()
+                v_cli = VKClient(config.vk_access_token)
+                post = v_cli.get_mostly_liked_post(oldest_group, after=after)
+                if post:
+                    group_profile = v_cli.get_group_profile(oldest_group)
+                    await send_method(post, group_profile)
+                    s.set_after(oldest_group, post['date'])
+                    return
+                else:
+                    s.set_after(oldest_group, datetime.datetime.now().timestamp())
+                    log('Отсутствуют новые подходящие посты:', oldest_group)
+
+    async def send_embed_from_discord(self, message):
+        embed = prepare_embed_from_discord(message)
+        await self.discord_to_discord_channel.send(embed=embed)
+
+    async def set_game(self):
+        activity = discord.Activity(name=config.game_name, type=discord.ActivityType.listening)
+        await self.change_presence(activity=activity, status=discord.Status.online)
+
+    async def on_ready(self):
+        log('Вошёл под', self.user)
+        self.discord_channel = self.get_channel(config.discord_channel_id)
+        self.discord_to_discord_channel = self.get_channel(config.discord_to_discord_channel_id)
+        await self.set_game()
+
+    async def vk_task(self):
+        await self.wait_until_ready()
+        await asyncio.sleep(5)
+        while not self.is_closed():
+            try:
+                await self.get_vk_data(self.send_embed_from_vk)
+            except Exception as e:
+                log(e)
+            await asyncio.sleep(config.vk_to_discord_timeout)
+
+    async def on_message(self, message):
+        await self.wait_until_ready()
+        if message.channel.id not in config.discord_channel_ids:
+            return
+        if self.user == message.author:
+            return
+        await self.send_embed_from_discord(message)
+
+keep_alive()
+
+
+intents = discord.Intents.none()
+intents.guilds = True
+intents.guild_messages = True
+
+client = GillyClient(intents=intents)
+client.run(config.discord_token)
